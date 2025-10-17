d53bb40062fd2d8f5af92576333a58ab1a5e42e9
nvila-iso <nvila88@proton.me>
2025-10-17 17:09:08 -0400

    adjustments for Controls page (mobile)

--- src/components/Handheld/Controls.jsx
+++ src/components/Handheld/Controls.jsx
@@ -4,10 +4,9 @@ const Controls = () => {
   return (
     <>
       <div className="h-90 p-1">
-
         <p className="text-center text-lg">CONTROLS</p>
         <div className="border-2 border-zinc-500 p-1 rounded-md h-[92%]">
-          <form className="flex flex-col gap-5">
+          <form className="flex flex-col gap-3">
             <fieldset className="border-1 border-zinc-500 text-sm rounded-sm">
               <legend className="font-semibold">Development</legend>
               <p className="px-1">
@@ -20,25 +19,30 @@ const Controls = () => {
                 Windows + Linux OS, Computer Hardware, Networking
               </p>
             </fieldset>
-
-            <fieldset className="border-1 border-zinc-500 text-sm rounded-sm w-50">
-              <legend className="font-semibold">Soft Skills</legend>
-              <p className="px-1">
-                Communication, Leadership, Teamwork, Problem Solving
-              </p>
-            </fieldset>
-            <fieldset className="border-1 border-zinc-500 text-sm rounded-sm w-50">
-              <legend className="font-semibold">Other Tools</legend>
-              <p className="px-1">
-                Adobe Suite, OBS Studio, Figma, Twitch & Youtube
-              </p>
-            </fieldset>
           </form>
-          <img
-            src={handheld}
-            alt=""
-            className="relative left-25 bottom-35 h-35 mx-auto opacity-60"
-          />
+          <div className="grid grid-cols-2 mt-1">
+            <div>
+              <form className="flex flex-col gap-3">
+                <fieldset className="border-1 border-zinc-500 text-sm rounded-sm">
+                  <legend className="font-semibold">Soft Skills</legend>
+                  <p className="px-1">
+                    Communication, Leadership, Teamwork, Problem Solving
+                  </p>
+                </fieldset>
+                <fieldset className="border-1 border-zinc-500 text-sm rounded-sm">
+                  <legend className="font-semibold">Other Tools</legend>
+                  <p className="px-1">
+                    Adobe Suite, OBS Studio, Figma, Twitch & Youtube
+                  </p>
+                </fieldset>
+              </form>
+            </div>
+            <img
+              src={handheld}
+              alt=""
+              className="h-35 mx-auto opacity-60"
+            />
+          </div>
         </div>
       </div>
     </>
