8474132988031803216c9148254c8b5a196bf87e
nvila-iso <nvila88@proton.me>
2025-10-18 12:49:13 -0400

    minor adjustment on grid size for controls

--- src/components/Handheld/Controls.jsx
+++ src/components/Handheld/Controls.jsx
@@ -22,7 +22,7 @@ const Controls = () => {
             </fieldset>
           </form>
           <div
-            className="grid grid-cols-[minmax(100px,_1fr)_minmax(60px,_100px)]
+            className="grid grid-cols-[minmax(100px,_1fr)_minmax(30px,_60px)]
  mt-4 gap-1 items-center"
           >
             <div>
