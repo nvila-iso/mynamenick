fe798c6a9d33c45810fc827f8d23713f4b0be7a3
nvila-iso <nvila88@proton.me>
2025-10-20 20:12:55 -0400

    added pdf buttons on manual page, added color pallete above main page.

--- src/components/Flipbook/Flipbook.jsx
+++ src/components/Flipbook/Flipbook.jsx
@@ -20,30 +20,49 @@ const Flipbook = () => {
   }, []);
 
   return (
-    <div className="mx-auto flex justify-center flipbook">
-      <HTMLFlipBook
-        key={usePortrait ? "portrait" : "landscape"}
-        ref={flipRef}
-        size="fixed"
-        width={640}
-        height={600}
-        showCover
-        usePortrait={usePortrait}
-        flippingTime={900}
-        maxShadowOpacity={0.1}
-        mobileScrollSupport
-        className=""npm
-      >
-        <CoverPage />
-        <PageOne />
-        <PageTwo />
-        <PageThree />
-        <PageFour />
-        <PageFive />
-        <PageSix />
-        <BackCover />
-      </HTMLFlipBook>
-    </div>
+    <>
+      <div className="mx-auto flex justify-center flipbook">
+        <HTMLFlipBook
+          key={usePortrait ? "portrait" : "landscape"}
+          ref={flipRef}
+          size="fixed"
+          width={640}
+          height={600}
+          showCover
+          usePortrait={usePortrait}
+          flippingTime={900}
+          maxShadowOpacity={0.1}
+          mobileScrollSupport
+          className=""
+          npm
+        >
+          <CoverPage />
+          <PageOne />
+          <PageTwo />
+          <PageThree />
+          <PageFour />
+          <PageFive />
+          <PageSix />
+          <BackCover />
+        </HTMLFlipBook>
+      </div>
+      <div className="absolute w-full bottom-10 flex justify-center">
+        <div className="flex gap-2">
+          <button className="bg-white flex flex-col p-2 rounded-md shadow-[-5px_5px_0px_rgba(0,0,0,1)] hover:shadow-[-5px_-5px_0px_rgba(0,0,0,1)] transition-all">
+            Boring Resume
+          </button>
+          <button
+            className="bg-white flex flex-col p-2 rounded-md shadow-[0px_5px_0px_rgba(0,0,0,1)] hover:shadow-[0px_-5px_0px_rgba(0,0,0,1)] transition-all"
+            onClick={() => window.open("/pdf/PDF_Manual.pdf", "_blank")}
+          >
+            PDF Manual
+          </button>
+          <button className="bg-white flex flex-col p-2 rounded-md shadow-[5px_5px_0px_rgba(0,0,0,1)] hover:shadow-[5px_-5px_0px_rgba(0,0,0,1)] transition-all">
+            Foldable Manual
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import Nav from "./Nav";
 
 const RootLayout = () => {
   return (
     <>
+      <Nav/>
       <main className="min-h-screen w-full grid place-items-center bg-[#DE954D] shadow-md">
         <Outlet />
       </main>
